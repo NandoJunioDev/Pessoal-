9ccc44f071f4d8d18b9fc704cfe8fc9445e03964
NandoJunioDev <frnndmaz_cf@outlook.com>
2024-07-28 20:00:58 -0300

    28/07 small att

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log ("Voce é muito gay");
+console.log ("Voce é muito gay"); 
+console.log(Message);
+let n = 10
+for ( i=0; i<=n; i++){
+
+    console.log("Voce é gay")
+
+    if( i==5 ){
+        console.log("Vocé muito gay ")
+    }
+
+    
+
+}
+
